ba9b642b3cd6d2bd05ec06a5d2fd9854007221fe
Adriel Francisco Rocha <67209277+Adriel-Rocha@users.noreply.github.com>
2026-01-31 12:25:30 +0000

    adiciona scripts `lint:check` e `lint:ffix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Clone-tab-news
+
 Projeto realizado a partir das aulas do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mãe eu amo você. Se a senhora me ama, dá uma risadinha</h1>
+  return <h1>Mãe eu amo você. Se a senhora me ama, dá uma risadinha</h1>;
 }
 
 export default Home;
