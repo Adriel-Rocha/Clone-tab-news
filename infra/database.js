1c7f7d9c34dd7551f067dfe9fabed2405e638def
Adriel Francisco Rocha <67209277+Adriel-Rocha@users.noreply.github.com>
2026-02-26 14:02:29 +0000

    add `api/v1/migrations` endpoint

--- infra/database.js
+++ infra/database.js
@@ -1,16 +1,11 @@
 import { Client } from 'pg';
 
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-  });
+  
+  let client;
   
   try {
-    await client.connect();
+    client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
 
@@ -23,6 +18,30 @@ async function query(queryObject) {
   }
 }
 
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    ssl: getSSLValues(),
+  });
+
+  await client.connect();
+  return client;
+}
+
 export default {
-  query: query,
+  query,
+  getNewClient,
+}
+function getSSLValues() {
+  if(process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  
+  return process.env.NODE_ENV === "production" ? true : false;
 }
