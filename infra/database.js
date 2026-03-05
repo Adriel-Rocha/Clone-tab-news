f3652c9a52db46b34bb5e11bdac6881cce4f3140
Adriel Francisco Rocha <67209277+Adriel-Rocha@users.noreply.github.com>
2026-03-05 08:31:56 +0000

    fix `prettier` style linting

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,17 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
 async function query(queryObject) {
-  
   let client;
-  
+
   try {
     client = await getNewClient();
     const result = await client.query(queryObject);
     return result;
-
   } catch (error) {
     console.error(error);
     throw error;
   } finally {
     await client.end();
-
   }
 }
 
@@ -35,13 +32,13 @@ async function getNewClient() {
 export default {
   query,
   getNewClient,
-}
+};
 function getSSLValues() {
-  if(process.env.POSTGRES_CA) {
+  if (process.env.POSTGRES_CA) {
     return {
       ca: process.env.POSTGRES_CA,
     };
   }
-  
+
   return process.env.NODE_ENV === "production" ? true : false;
 }
